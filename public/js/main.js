31f188a2999e5a1b4b3d423ee39a0ac56c7fa4db
Michael O'Mahoney <mjvomahoney@gmail.com>
2024-10-01 15:57:04 -0500

    Added image zoom

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems);
+    var sideNavEl = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(sideNavEl);
+
+    var mBoxEl = document.querySelectorAll('.materialboxed');
+    var instances = M.Materialbox.init(mBoxEl); 
 });
