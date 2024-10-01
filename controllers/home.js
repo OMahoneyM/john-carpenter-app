7193f2942e339319a091fe3680df04f2006b58b7
Michael O'Mahoney <mjvomahoney@gmail.com>
2024-10-01 16:09:01 -0500

    Added comments

--- controllers/home.js
+++ controllers/home.js
@@ -3,8 +3,9 @@ const Film = require('../models/Film')
 module.exports = {
     getIndex: async (req, res) => {
         try {
+            //Store every doc from film collection in array
             const filmList = await Film.find({})
-            //console.log(filmList)
+            //Pass filmList array into index.ejs view
             res.render('index.ejs', {films: filmList}) 
         } catch (err) {
             console.error(err)
